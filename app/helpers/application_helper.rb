54573e4373dd01a45408c26a72406386d7f738f0
VSIS <oh@my.god>
2016-02-16 18:37:56 +0300

    'finish'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def city_list
+        names = City.all.pluck(:name)
+        html = ''
+        names.each do |name|
+            html += "<div>#{name}</div>"
+        end
+        html
+    end
 end
